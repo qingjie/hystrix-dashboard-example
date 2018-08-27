d3acdb008b76650c729e8955c9212a2eb825784e
bharatbolisetty <bharatbolisetty@users.noreply.github.com>
2018-08-27 06:55:27 +0530

    throw error with data

--- src/main/java/com/techprimers/hystrixdashboardexample/RestResource.java
+++ src/main/java/com/techprimers/hystrixdashboardexample/RestResource.java
@@ -1,28 +1,37 @@
 package com.techprimers.hystrixdashboardexample;
 
+import com.google.common.collect.Maps;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.apache.commons.lang.math.RandomUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.ws.Response;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/rest")
 public class RestResource {
 
-    @HystrixCommand(fallbackMethod = "fallBackHello",
-            commandKey = "hello1", groupKey = "hello11")
+    @HystrixCommand(commandKey = "hello1", groupKey = "hello11")
     @GetMapping("/hello1")
-    public String hello() {
+    public ResponseEntity<Map<String, String>> hello() {
+        Map<String, String> response = Maps.newHashMap();
         //Wrong
         if (RandomUtils.nextBoolean()) {
-            throw new RuntimeException("Failed!");
+            response.put("errorCode", "FAILED_400");
+            response.put("message", "error!!");
+            response.put("status", "400");
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
         }
-        return "Hello World 1";
+        response.put("message", "success");
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @HystrixCommand(fallbackMethod = "fallBackHello",
-            commandKey = "hello2", groupKey = "hello22")
+    @HystrixCommand(commandKey = "hello2", groupKey = "hello22")
     @GetMapping("/hello2")
     public String hello2() {
         //Wrong
